223e974d7592ce2dfcf828e7af9aa5543297471c
JenilGajjar20 <68738624+JenilGajjar20@users.noreply.github.com>
2021-04-20 21:53:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,31 +31,44 @@ class Account:
 
 
 def main():
+    # Creating accounts     
     accounts = []
     for i in range(1000, 9999):
         account = Account(i, 0)
         accounts.append(account)
 
+    # ATM processes
     while True:
+        # Reading id from user
         id = int(input("Enter your pin: "))
-
-        if id < 1000 or id > 9999:
+        
+        # Loop till id is valid        
+        while id < 1000 or id > 9999:
             id = int(input("Invalid id...! Re-enter: "))
 
+            # Iterating over account session
             while True:
+                # Printing options
                 print("\t1. Balance \t2. Withdraw amount \t3. Deposit amount \t4. Exit")
-
+                
+                # Reading options
                 option = int(input("Enter your choice: "))
 
+                # Getting account object     
                 for acc in accounts:
+                    # Comparing account id
                     if acc.getId() == id:
                         accountObj = acc
                         break
 
+                # View Balance
                 if option == 1:
+                    # Printing balance
                     print(f"Your current balance is: {accountObj.getBalance()}")
 
+                # Withdraw
                 elif option == 2:
+                    # Reading amount
                     withdrawAmount = float(input("Enter your amount to withdraw: "))
                     verifyWithdraw = input(f"Are you sure you want to withdraw {withdrawAmount} from the account? Yes/No: ")
 
@@ -65,23 +78,30 @@ def main():
                         break
 
                     if withdrawAmount < accountObj.getBalance():
+                        # Calling withdraw method
                         accountObj.withdraw(withdrawAmount)
+                        # Printing updated balance
                         print(f"Your updated balance is {accountObj.getBalance()}")
                     else:
                         print(f"Your balance is less than the withdrawal amount you entered {accountObj.getBalance()}\n")
                         print(f"Please make a deposit")
 
+                # Deposit
                 elif option == 3:
+                    # Reading amount
                     depositAmount = float(input("Enter your amount to deposit: "))
                     verifyDeposit = input(f"Are you sure you want to deposit {depositAmount} into the account? Yes/No: ")
 
                     if verifyDeposit == 'yes':
                         print("Verified deposit")
+                        # Calling deposit method
                         accountObj.deposit(depositAmount)
+                        # Printing updated balance
                         print(f"Your updated balance is {accountObj.getBalance()}")
                     else:
                         break
 
+                # Exit
                 elif option == 4:
                     print("Transaction is now complete")
                     print(f"Transaction number: {random.randint(10000, 1000000)}")
@@ -89,9 +109,10 @@ def main():
                     print(f"Monthly Interest Rate: {accountObj.annualInterestRate / 12}")
                     print(f"Thank you for choosing us as your bank.")
                     exit()
-
+                
+                # Any other choice
                 else:
                     print("Please enter valid choice!")
 
-
+# main function
 main()
